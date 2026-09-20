efbe0e4fa65feb097683d3f072f22a40b1e42426
mudeermukhtar-dotcom <mudeermukhtar@gmail.com>
2026-09-20 15:52:28 +0530

    addday9.py
    day 9 of learning python

---
+++ day9.py
@@ -0,0 +1,96 @@
+#Write a Python program to count how many numbers in a list are greater than 10:
+'''numbers = [12, 7, 24, 9, 16, 31, 8]
+count=0
+for i in numbers:
+    if i > 10:
+        count=count+1
+print(count)'''
+#Write a Python program to find the sum of all even numbers in a list.
+'''numbers = [12, 7, 24, 9, 16, 31, 8]
+sum=0
+for i in numbers:
+    if i % 2== 0:
+        sum=sum+i
+print(sum)'''
+#Write a Python program to count how many odd numbers are present in a list.
+'''numbers = [15, 8, 21, 6, 11, 4, 19]
+count=0
+for i in numbers:
+    if  i % 2 != 0:
+        count=count+1
+print(count)'''
+#write a Python program to find the sum of all odd numbers in the list.
+'''numbers = [11, 4, 7, 10, 15, 8, 3]
+total=0
+for i in numbers:
+    if i % 2 != 0:
+        total=total+i
+print(total)'''
+#Write a Python program to find the smallest number in the list without using min().
+'''numbers = [25, 12, 8, 31, 6, 19]
+smallest=numbers[0]
+for i in numbers:
+    if i < smallest:
+        smallest=i
+print(smallest)'''
+#Write a Python program to find the largest number in the list :
+'''numbers = [25, 12, 8, 31, 6, 19]
+largest=numbers[0]
+for i  in numbers:
+    if i > largest:
+        largest=i
+print(largest)'''
+#Write a Python program to count how many numbers are divisible by 3 in the list.
+'''numbers = [6, 10, 12, 17, 21, 25, 30]
+count=0
+for i in numbers:
+    if i % 3==0:
+        count=count+1
+print(count)'''
+#Write a Python program to calculate the total of all numbers in the list without using
+'''numbers = [6, 10, 12, 17, 21, 25, 30]
+sum=0
+for i in numbers:
+    sum=sum+i
+print(sum)'''
+#Write a Python program to count how many even and odd numbers are present in the list
+'''numbers = [4, 7, 12, 9, 15, 20, 8]
+counte=0
+counto=0
+for i in numbers:
+    if i % 2  ==0:
+     counte=counte+1
+    elif i % 2 != 0: 
+     counto=counto+1
+print("even numbers",counte,"odd numbers",counto)'''
+#Write a Python program to find the second number in the list.
+'''numbers = [10, 20, 30, 40, 50]
+print(numbers[1])'''
+#Write a Python program to find the average of all numbers in the list :
+'''numbers = [10, 20, 30, 40, 50]
+total=0
+for i in numbers:
+    total=total+i
+average=total/len(numbers)
+print(average)'''
+#Write a Python program to find how many times the number 5 appears in the list
+'''numbers = [5, 2, 5, 8, 5, 10, 3, 5]
+count=0
+for i in numbers:
+    if  i ==5:
+        count=count+1
+print(count)'''
+#Write a Python program to find the sum of all numbers greater than 10 in the list
+'''numbers = [5, 12, 8, 20, 15, 3, 25]
+sum=0
+for i in numbers:
+    if i > 10:
+        sum=sum+i
+print(sum)'''
+#Write a Python program to print the numbers that are divisible by both 2 and 3.
+'''numbers = [6, 10, 12, 15, 18, 20, 24, 25]
+for i in numbers:
+    if i % 2 ==0 and i % 3== 0:
+     print(i)'''
+ 
+    
